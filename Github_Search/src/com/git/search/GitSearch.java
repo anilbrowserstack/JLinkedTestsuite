2c859783e846fbe41421f5874dc8a16c2c629f08
Anil Kumar Puvathumkadavil <anil@browserstack.com>
2013-06-19 22:13:21 +0530

    Updated GitSearch.java

--- Github_Search/src/com/git/search/GitSearch.java
+++ Github_Search/src/com/git/search/GitSearch.java
@@ -8,21 +8,26 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class GitSearch {
 	//WebDriver driver = new ChromeDriver();
+	//WebDriver driver = new FirefoxDriver();
 	WebDriver driver = null;
 	JavascriptExecutor js = null;
 	
 	void githubSearch(String emailid, String pc) throws InterruptedException, IOException{
-		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-	    //capabilities.setVersion("26.0");
-	    //capabilities.setPlatform(Platform.WINDOWS);
-	    capabilities.setPlatform(Platform.MAC);
-	    capabilities.setCapability("build", "BUILD_SEL_TEST");
-	    capabilities.setCapability("project", "PROJECT_SEL_TEST");
+		//DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+//	    capabilities.setVersion("7");
+	    capabilities.setPlatform(Platform.WINDOWS);
+	    capabilities.setBrowserName("internet explorer");
+	    //capabilities.setPlatform(Platform.MAC);
+	    //capabilities.setCapability("browserstack.debug","true");
+	    capabilities.setCapability("build", "HUB_SEL1_TEST");
+	    capabilities.setCapability("project", "HUB_SEL1_TEST");
 		//hub.browserstack
 	    driver = new RemoteWebDriver(new URL("http://AnilKumar4:88a9f5607d78f757481450d52f7e4cb7@hub.browserstack.com:4444/wd/hub"), capabilities);
 		//my m/c
@@ -31,9 +36,15 @@ public class GitSearch {
 		//driver = new RemoteWebDriver(new URL("http://192.168.0.104:4444/wd/hub"), capabilities);
 	    //sauce labs
 	    //driver = new RemoteWebDriver(new URL("http://utsavk:2fb97838-fac2-4780-b4f0-586b7f061944@ondemand.saucelabs.com:80/wd/hub"), capabilities);
-	    
+	    //fuhub.browserstack with wtf credentials
+	    //driver = new RemoteWebDriver(new URL("http://anil8:ff73fcee5c3179fe480e3b27e8a7254b@fuhub.browserstack.com:4444/wd/hub"), capabilities);
+	    //fuhub anil.keralite
+	    //driver = new RemoteWebDriver(new URL("http://anil:eea464767f429ab13bd3e4d3330db67c@fuhub.browserstack.com:4444/wd/hub"), capabilities);
+	    //fuhub automationbs+6@gmail.com
+	    //driver = new RemoteWebDriver(new URL("http://BSAutomation8:5c7b1ebc567b22a05359d210be4e1ff6@fuhub.browserstack.com:4444/wd/hub"), capabilities);
+
 		driver.get("https://github.com/search?q=location%3Amumbai&type=Users&ref=advsearch&l=");
-		Thread.sleep(10000);
+		//Thread.sleep(10000);
 		if (driver instanceof JavascriptExecutor) {
 		    js = (JavascriptExecutor)driver;
 		}
